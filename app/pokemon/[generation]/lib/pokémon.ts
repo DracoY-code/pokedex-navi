8c33255e0a214f167284c512c31c2dbabc08ea5e
Yashvardhan Singh <draco882003@gmail.com>
2024-09-09 18:52:38 +0530

    Relocate contents of `generatePokémonList` to `page.tsx`

--- app/pokemon/[generation]/lib/pokémon.ts
+++ app/pokemon/[generation]/lib/pokémon.ts
@@ -40,49 +40,6 @@ export async function fetchPokémonDetails(id: number): Promise<Pokémon> {
   }
 }
 
-// Function to generate the list with Pokémon details for the selected generation
-export async function generatePokémonList(
-  generation: number = 1
-): Promise<Pokémon[]> {
-  // Mapping for the number of Pokémon in each generation
-  const generationalMapping: Record<number, { limit: number; offset: number }> =
-    {
-      1: { limit: 151, offset: 0 },
-      2: { limit: 100, offset: 151 },
-      3: { limit: 135, offset: 251 },
-      4: { limit: 107, offset: 386 },
-      5: { limit: 156, offset: 493 },
-      6: { limit: 72, offset: 649 },
-      7: { limit: 88, offset: 721 },
-      8: { limit: 96, offset: 809 },
-      9: { limit: 120, offset: 905 },
-    };
-
-  // Fetch the list of Pokémon for the selected generation
-  const listResponse = await fetchPokémonList(
-    generationalMapping[generation].limit,
-    generationalMapping[generation].offset
-  );
-
-  return Promise.all(
-    // Fetch the details for each Pokémon in that generation
-    listResponse.results.map(async (result) => {
-      // Extracting the ID from the URL
-      const urlPaths = result.url.split("/");
-      const id = Number(urlPaths[urlPaths.length - 2]);
-
-      // Handle invalid ID
-      if (isNaN(id)) {
-        console.error(`Pokémon ID (${id}) not found`);
-        return notFound();
-      }
-
-      // Fetch the Pokémon details with the ID
-      return fetchPokémonDetails(id);
-    })
-  );
-}
-
 // Function to fetch details for the Pokémon abilities
 export async function fetchPokémonAbilities(
   id: number
--- app/pokemon/[generation]/page.tsx
+++ app/pokemon/[generation]/page.tsx
@@ -2,7 +2,14 @@ import { notFound } from "next/navigation";
 import GenerationTabList from "./components/GenerationTabList";
 import PokémonLister from "./components/PokémonLister";
 import { Pokémon } from "./lib/interfaces";
-import { generatePokémonList } from "./lib/pokémon";
+import { fetchPokémonDetails, fetchPokémonList } from "./lib/pokémon";
+
+// Function to generate static paths for each generation
+export async function generateStaticParams() {
+  return Array.from({ length: 9 }, (_, index) => ({
+    generation: `${index + 1}`,
+  }));
+}
 
 export default async function PokémonListPage({
   params,
@@ -17,13 +24,46 @@ export default async function PokémonListPage({
     return notFound();
   }
 
-  // Fetch the Pokémon list for the selected generation
-  const pokémonList: Pokémon[] = await generatePokémonList(generation);
+  // Mapping for the number of Pokémon in each generation
+  const generationalMapping: Record<number, { limit: number; offset: number }> =
+    {
+      1: { limit: 151, offset: 0 },
+      2: { limit: 100, offset: 151 },
+      3: { limit: 135, offset: 251 },
+      4: { limit: 107, offset: 386 },
+      5: { limit: 156, offset: 493 },
+      6: { limit: 72, offset: 649 },
+      7: { limit: 88, offset: 721 },
+      8: { limit: 96, offset: 809 },
+      9: { limit: 120, offset: 905 },
+    };
+
+  const listResponse = await fetchPokémonList(
+    generationalMapping[generation].limit,
+    generationalMapping[generation].offset
+  );
+
+  const pokémonList: Pokémon[] = await Promise.all(
+    listResponse.results.map(async (result) => {
+      // Extracting the ID from the URL
+      const urlPaths = result.url.split("/");
+      const id = Number(urlPaths[urlPaths.length - 2]);
+
+      // Handle invalid ID
+      if (isNaN(id)) {
+        console.error(`Pokémon ID (${id}) not found`);
+        return notFound();
+      }
+
+      // Fetch the Pokémon details with the ID
+      return fetchPokémonDetails(id);
+    })
+  );
 
   return (
     <main>
-      <GenerationTabList params={{ generation: generation }} />
-      <PokémonLister params={{ pokémonList: pokémonList }} />
+      <GenerationTabList params={{ generation }} />
+      <PokémonLister params={{ pokémonList }} />
     </main>
   );
 }
